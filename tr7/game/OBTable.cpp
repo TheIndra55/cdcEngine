ad1e796535084aa2d31a15d184e8d77a68b69be6
Indra <wuppie55@gmail.com>
2023-08-03 22:17:13 +0200

    Implement OBTABLE_Init and OBTABLE_GetObjectID

--- tr7/game/OBTable.cpp
+++ tr7/game/OBTable.cpp
@@ -4,10 +4,113 @@
 #include "OBTable.h"
 #include "Main.h"
 
+ObjectList* gObjectList;
+
+inline char* getline(char* str, char* text)
+{
+	if (*text != '\n')
+	{
+		while (*text != '\r')
+		{
+			*str++ = *text++;
+
+			if (*text == '\n')
+			{
+				text++;
+				break;
+			}
+		}
+	}
+
+	while (*text == '\n' || *text == '\r')
+	{
+		text++;
+	}
+
+	*str = 0;
+
+	return text;
+}
+
 void OBTABLE_Init()
 {
 	char objectListFileName[80];
 	sprintf(objectListFileName, "\\TR7\\%s\\objectlist.txt", ActiveBuildName);
 
 	auto listText = FSHelper_ReadFile(objectListFileName, 0, GetFS());
+
+	char str[512];
+	listText = getline(str, listText);
+
+	int numObjects;
+	sscanf(str, "%d", &numObjects);
+
+	char* start = listText;
+	int largest = 0;
+
+	for (int i = 0; i < numObjects; i++)
+	{
+		listText = getline(str, listText);
+
+		int dummy;
+		sscanf(str, "%d,", &dummy);
+
+		if (dummy > largest)
+		{
+			largest = dummy;
+		}
+	}
+
+	if (gObjectList)
+	{
+		free(gObjectList);
+	}
+
+	gObjectList = (ObjectList*)malloc(sizeof(ObjectList) + (largest * sizeof(ObjectEntry)) + (listText - start));
+	gObjectList->numObjects = largest;
+
+	for (int i = 0; i < largest; i++)
+	{
+		gObjectList->object[i].name = nullptr;
+		gObjectList->object[i].trackerIndex = -1;
+	}
+
+	listText = start;
+	char* names = (char*)&gObjectList->object[largest];
+
+	for (int i = 0; i < numObjects; i++)
+	{
+		listText = getline(str, listText);
+
+		ObjectEntry* object = &gObjectList->object[i];
+
+		sscanf(str, "%d,%s", &object->trackerIndex, names);
+		object->name = names;
+
+		char* c = strchr(names, ',');
+		if (c)
+		{
+			*c = 0;
+		}
+
+		names += strlen(names) + 1;
+	}
+}
+
+int OBTABLE_GetObjectID(char* name)
+{
+	if (gObjectList->numObjects <= 0)
+	{
+		return 0;
+	}
+
+	for (int i = 0; i < gObjectList->numObjects; i++)
+	{
+		if (strcmp(name, gObjectList->object[i].name) == 0)
+		{
+			return gObjectList->object[i].trackerIndex;
+		}
+	}
+
+	return 0;
 }
--- tr7/game/OBTable.h
+++ tr7/game/OBTable.h
@@ -1,3 +1,16 @@
 #pragma once
 
+struct ObjectEntry
+{
+	char* name;
+	int trackerIndex;
+};
+
+struct ObjectList
+{
+	int numObjects;
+	ObjectEntry object[1];
+};
+
 void OBTABLE_Init();
+int OBTABLE_GetObjectID(char* name);
